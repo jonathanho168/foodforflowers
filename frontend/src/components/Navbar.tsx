c3dfc18f0217105f8dabbb74eaa8a0c5724fce59
Matthew Wang <mslwang2001@gmail.com>
2023-02-18 21:14:41 -0800

    Removed unnecessary code

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -35,17 +35,14 @@ const NavLink = ({ children }: { children: ReactNode }) => (
 );
 
 export default function Navbar() {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-
     return (
         <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-            <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+            <Flex h={20} justifyContent={'space-between'}>
                 <Flex alignItems={'center'}>
                     <HStack
                         as={'nav'}
                         spacing={4}
-                        display={'flex'}
-                    >
+                        display={'flex'}>
                         <NavLink key={'Home'}>{'Home'}</NavLink>
                         <Button
                             variant={'solid'}
@@ -56,42 +53,8 @@ export default function Navbar() {
                         </Button>
                         <NavLink key={'Profile'}>{'Profile'}</NavLink>
                     </HStack>
-                    {/* <Flex alignItems={'center'}>
-
-                    <Menu>
-                        <MenuButton
-                            as={Button}
-                            rounded={'full'}
-                            variant={'link'}
-                            cursor={'pointer'}
-                            minW={0}>
-                            <Avatar
-                                size={'sm'}
-                                src={
-                                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                                }
-                            />
-                        </MenuButton>
-                        <MenuList>
-                            <MenuItem>Link 1</MenuItem>
-                            <MenuItem as={Button}>Link 2</MenuItem>
-                            <MenuDivider />
-                            <MenuItem>Link 3</MenuItem>
-                        </MenuList>
-                    </Menu>
-                </Flex> */}
                 </Flex>
             </Flex>
-
-            {/* {isOpen ? (
-                <Box pb={4} display={{ md: 'none' }}>
-                    <Stack as={'nav'} spacing={4}>
-                        {Links.map((link) => (
-                            <NavLink key={link}>{link}</NavLink>
-                        ))}
-                    </Stack>
-                </Box>
-            ) : null} */}
         </Box>
     );
 }
