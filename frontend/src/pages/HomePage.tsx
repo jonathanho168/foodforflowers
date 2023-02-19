29676ef49ae3bc60faeb3cd817aff48272813beb
Matthew Wang <mslwang2001@gmail.com>
2023-02-19 08:57:08 -0800

    Add Rewards modal to home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,19 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex, Icon, IconButton, useDisclosure } from "@chakra-ui/react";
 import React from "react";
 import Navbar from "../components/Navbar";
+import GroupIcon from '@mui/icons-material/Group';
+import RewardModal from "../components/RewardModal";
 
 const HomePage: React.FC = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
-        <Flex className="container" flexDirection={"column"} h="100vh">
+        <>
+            <Flex justifyContent={'space-between'} zIndex="1">
+                <Button onClick={onOpen}> Check Update </Button>
+                <IconButton icon={<Icon as={GroupIcon} />} aria-label={""}></IconButton>
+            </Flex>
+            <RewardModal isOpen={isOpen} onClose={onClose} />
+            <Flex className="container" flexDirection={"column"} h="100vh" zIndex="2">
             <div className="glitch-embed-wrap" style={{ height: "calc(100% - 60px)", width: "100%" }}>
                 <iframe
                     src="https://glitch.com/embed/#!/embed/meteor-rune-lyre?path=.env&previewSize=100"
@@ -14,10 +23,12 @@ const HomePage: React.FC = () => {
                     style={{ height: "100%", width: "100%", border: "0" }}>
                 </iframe>
             </div>
-            <nav className="navbar">
-                <Navbar />
-            </nav>
-        </Flex>
+                <nav className="navbar">
+                    <Navbar />
+                </nav>
+            </Flex>
+
+        </>
     );
 };
 
