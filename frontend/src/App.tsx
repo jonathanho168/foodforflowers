18ea002ac8d0eef0e112f74b73f6254a4324bff5
Matthew Wang <mslwang2001@gmail.com>
2023-02-18 16:48:26 -0800

    Convert to tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h3>This is the data you can find from our table:</h3>
+    </div>
+  );
+}
+
+export default App;
